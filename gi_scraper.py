655ab89d2c554b0205a05cf906fdf73ef0036f69
BruParis <parisbruno85@gmail.com>
2019-09-25 17:41:08 +0200

    fix on multiprocess + formatting

--- gi_scraper.py
+++ gi_scraper.py
@@ -13,16 +13,17 @@ req_header = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.22 (KHTML, like Ge
 
 # Images
 search_words = ['hotdog', 'not hotdog']
-num_images = [20, 30]
+num_images = [10, 30]
 start_idx = [0, 0]
 
+
 def write_img_file(img_item):
-  req = urllib.request.Request(img_item[1])
-  req.add_header('User-Agent', req_header)
-  raw_img = urllib.request.urlopen(req).read()
-  f = open(img_item[0], "wb")
-  f.write(raw_img)
-  f.close()
+    req = urllib.request.Request(img_item[1])
+    req.add_header('User-Agent', req_header)
+    raw_img = urllib.request.urlopen(req).read()
+    f = open(img_item[0], "wb")
+    f.write(raw_img)
+    f.close()
 
 def get_images(driver, folder_path, num):
     count = 0
@@ -31,26 +32,30 @@ def get_images(driver, folder_path, num):
     img_list = []
 
     for img in images:
-
-        if count > num:
-          break
+        if count >= num:
+            break
 
         img_url = json.loads(img.get_attribute('innerHTML'))["ou"]
         img_type = json.loads(img.get_attribute('innerHTML'))["ity"]
 
         if img_type not in extensions:
-          continue
+            continue
 
         file_name = folder_path + "/" + str(count) + "." + img_type
-        img_item = (file_name, img_url)
-        img_list.append(img_item)
+        img_list.append((file_name, img_url))
         count += 1
 
     pool = Pool()
-    for _ in tqdm.tqdm(pool.imap_unordered(write_img_file, img_list), total=len(img_list)):
-        pass
+    pbar = tqdm.tqdm(total=len(img_list))
+    def update(*a):
+        pbar.update()
+        # tqdm.write(str(a))
+
+    for i in range(pbar.total):
+        pool.apply_async(write_img_file, args=(img_list[i], ), callback=update)
     pool.close()
     pool.join()
+
 
 def scroll(driver, num_scrolls):
 
@@ -62,31 +67,36 @@ def scroll(driver, num_scrolls):
         # click "show more results"
         time.sleep(2.5)
         try:
-            driver.find_element_by_xpath("//input[@value='Plus de résultats']") .click()
+            driver.find_element_by_xpath(
+                "//input[@value='Plus de résultats']") .click()
             time.sleep(2.5)
         except Exception as e:
             print("    show more results failed -> exception: " + str(e))
 
+
 def search(search_txt, num, idx):
 
     if not os.path.exists(download_img_path):
-      os.makedirs(download_img_path)
+        os.makedirs(download_img_path)
 
     folder_path = download_img_path + search_txt.replace(" ", "_")
 
     # Create folders for each search
     if not os.path.exists(folder_path):
-      os.makedirs(folder_path)
+        os.makedirs(folder_path)
 
-    url = "https://www.google.co.in/search?q=" + search_txt + "&source=lnms&tbm=isch"
-    driver = webdriver.Chrome(executable_path=r"/usr/lib/chromium/chromedriver")
+    url = "https://www.google.co.in/search?q=" + \
+        search_txt + "&source=lnms&tbm=isch"
+    driver = webdriver.Chrome(
+        executable_path=r"/usr/lib/chromium/chromedriver")
     driver.get(url)
 
     # Google images -> 400 images before "show more results"
     # start scrolling to load enough images
-    num_scrolls = int((num + idx)/ 400 + 1)
+    num_scrolls = int((num + idx) / 400 + 1)
     scroll(driver, num_scrolls)
     get_images(driver, folder_path, num)
+
 
 for i in range(len(search_words)):
     w = search_words[i]
